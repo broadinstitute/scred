c79e50368b3ae11d1770786ccda920141c29a6cd
markjbaker <markdotbaker@gmail.com>
2019-04-01 09:52:36 -0400

    improved primitive dtypes and started project rep

--- scred/dtypes.py
+++ scred/dtypes.py
@@ -1,10 +1,67 @@
-from dataclasses import dataclass
+# dtypes.py
+
+# Defines classes whose instances represent various REDCap responses
+
+import re
+from typing import Collection
+
+import pandas as pd
+
+# ---------------------------------------------------
+
+# TODO: Make this configurable, not everyone uses our ID scheme
+# Maybe set it somewhere in config so it can be overwritten for
+# a project? Default to None, and only check if not None?
+ID_TEMPLATE = re.compile(r"[A-Z]{3}[1-9][0-9]{7}")
 
 class DataDictionary:
     pass
 
-class Record:
+
+class DataAccessGroup:
     pass
 
-class RecordSet:
-    pass
+
+class Record(pd.DataFrame):
+    """
+    Represents a single observation in a REDCap project.
+    """
+    def __init__(self, id_key: str, data: dict = dict()):
+        """
+        REDCap API will present a list of dicts; each dict is one record. We create a
+        record from a response dict and can handle bulk pulls with loops/RecordSet.
+        """
+        print("Creating a record instance...")
+        self._id = data[id_key]
+        super().__init__(data)
+    
+    @property
+    def id(self):
+        return self._id
+    
+    @id.setter
+    def id(self, value):
+        # Confirm the subject ID at least looks real
+        if not ID_TEMPLATE.match(value):
+            raise ValueError(f"Invalid ID: {value}")
+        self._id = value
+
+
+class RecordSet(pd.DataFrame):
+    def __init__(self, records: Collection[Record]):
+        """
+        Use the record IDs to build an Index.
+        """
+        # will this work if r.index is also multi?
+        idx = pd.MultiIndex([ (r.id, r.index) for r in records ])
+        # Make data a property..?
+        self._data = pd.DataFrame(index=idx)
+
+
+
+# breakthrough note: chained indexing problem is all about __getitem__.
+# so it's really about making sure assignment is directly calling __setitem__
+# on the object and never trusting what's returned to be a view or copy.
+
+# Implications for how I treat the .copy() method too. Probably don't need to
+# do that nearly as often as I am.
--- scred/webapi.py
+++ scred/webapi.py
@@ -1,10 +1,17 @@
-# webapi.py
+"""
+scred/webapi.py
 
-# Creates the request-sending class used to interact with a REDCap instance.
+Creates the request-sending class used to interact with a REDCap instance.
+"""
 
-# External dependencies
+# 3/27/19: Maybe have calls be methods in RedcapRequester, then merge this
+# with dtypes.py? Or have this be just for RedcapRequester (name goes back),
+# then put RedcapRequester in a separate module that pulls from both and 
+# can be kept separate from them? project.py
+# Sounds good right now, see how it looks in the morning. 
+
 import requests
-# Internal dependencies
+
 import config
 import dtypes
 
@@ -14,14 +21,15 @@ user_cfg = config.USER_CFG
 
 class RedcapRequester:
     def __init__(self, config.RedcapConfig: cfg = user_cfg):
-        self._config = cfg
+        self.url = cfg['url']
         self._payloader = __class__._build_payloader(cfg)
+        self._version = None
 
 
     @staticmethod
     def _build_payloader(cfg):
         def payloader(**kwargs):
-            '''Constructs the payload for a request.'''
+            """Constructs the payload for a request."""
             payload = {
                 'token': cfg['token'],
                 'url': cfg['url'],
@@ -34,7 +42,7 @@ class RedcapRequester:
 
     def send_post_request(self, **kwargs):
         payload = self._payloader(**kwargs)
-        response = requests.post(self._config['url'], payload)
+        response = requests.post(self.url, payload)
         if not response.ok:
             msg = (
                 "Couldn't complete request. Code "
@@ -44,39 +52,32 @@ class RedcapRequester:
         else:
             return response.json()
 
-
-    # should this be a property called `version`?
-    def get_redcap_version(self):
-        return self.send_post_request(content="version")
-
-
-    # NOTE: Return the JSON data, not the instance of DataDictionary
-    def get_data_dictionary(self):
-        pass
-        # TODO: Implement
-        # return dtypes.DataDictionary(response_data)
-
     
-    def get_export_fieldnames(self, fields=None):
-        '''
-        (From REDCap documentation)
-        This method returns a list of the export/import-specific version of field names for all fields
-        (or for one field, if desired) in a project. This is mostly used for checkbox fields because
-        during data exports and data imports, checkbox fields have a different variable name used than
-        the exact one defined for them in the Online Designer and Data Dictionary, in which *each checkbox
-        option* gets represented as its own export field name in the following format: field_name +
-        triple underscore + converted coded value for the choice. For non-checkbox fields, the export
-        field name will be exactly the same as the original field name. Note: The following field types
-        will be automatically removed from the list returned by this method since they cannot be utilized
-        during the data import process: 'calc', 'file', and 'descriptive'.
+    @property
+    def version(self):
+        # Don't make this call unless we need to
+        if self._version == None:
+            self.version = self.send_post_request(content='version')
+        return self._version
 
-        The list that is returned will contain the three following attributes for each field/choice:
-        'original_field_name', 'choice_value', and 'export_field_name'. The choice_value attribute
-        represents the raw coded value for a checkbox choice. For non-checkbox fields, the choice_value
-        attribute will always be blank/empty. The export_field_name attribute represents the export/import-
-        specific version of that field name.
-        '''
-        payload_kwargs = {"content": "exportFieldNames"}
-        if fields:
-            payload_kwargs.update(field=fields)
-        return self.send_post_request(payload_kwargs)
+    @version.setter
+    def version(self, value):
+        self._version = value
+
+
+    # Pattern 1: Methods attached to the class instance
+
+
+
+# ---------------------------------------------------
+
+# Pattern 2: Top-level functions taking project as argument
+# Update: These are all going to methods of Project, a higher-level
+# class that accesses RedcapRequester (or a config) and dtypes
+def get_redcap_version(project):
+    return project.send_post_request(content='version')
+
+
+def get_data_dictionary(project, fields = None):
+    return project.send_post_request(content='metadata')
+
