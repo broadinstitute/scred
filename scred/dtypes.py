53c83ce1b46a83bc927d33f6d78e222a8716c081
markjbaker <markdotbaker@gmail.com>
2019-04-02 16:08:04 -0400

    Added to docstring for DataDictionary

--- scred/dtypes.py
+++ scred/dtypes.py
@@ -1,6 +1,8 @@
-# dtypes.py
+"""
+dtypes.py
 
-# Defines classes whose instances represent various REDCap responses
+Defines classes whose instances represent various REDCap responses.
+"""
 
 import re
 from typing import Collection
@@ -14,9 +16,53 @@ import pandas as pd
 # a project? Default to None, and only check if not None?
 ID_TEMPLATE = re.compile(r"[A-Z]{3}[1-9][0-9]{7}")
 
-class DataDictionary:
-    pass
+class DataDictionary(pd.DataFrame):
+    """ 
+    Represents a REDCap Metadata/Data Dictionary object for a given project.
+    At least to start, we are only creating these from JSON data returned by API.
+    So we work from a list of dicts.
 
+    Columns: actual_name_in_returned_json
+        Variable / Field Name: field_name
+        Form Name: form_name
+        Section Header: section_header
+        Field Type: field_type
+        Field Label: field_label
+        Choices, Calculations, OR Slider Labels: select_choices_or_calculations
+        Field Note: field_note
+        Text Validation Type OR Show Slider Number: text_validation_type_or_show_slider_number
+        Text Validation Min: text_validation_min
+        Text Validation Max: text_validation_max
+        Identifier?: identifier
+        Branching Logic (Show field only if...): branching_logic
+        Required Field?: required_field
+        Custom Alignment: custom_alignment
+        Question Number (surveys only): question_number
+        Matrix Group Name: matrix_group_name
+        Matrix Ranking?: matrix_ranking
+        Field Annotation: field_annotation
+    """
+    def __init__(self, rawdata):
+        super().__init__(rawdata)
+    
+    def add_response(self, field, response):
+        """
+        field can be a single field (str) or container of several (set, list, etc).
+        response is a map.
+        """
+        # Put new responses into right format
+        notdone = ""
+        for code, label in response.items():
+            notdone += f"{code}, {label} | "
+            # Remember that we only need first | if options already exist
+        # If only one field given, put it into a list
+        if isinstance(field, str):
+            field = [field]
+        for somefield in field:
+            choices = self.loc[ self['field_name']==field, 'select_choices_or_calculations' ]
+            # TODO: Finish implementing
+            # add new choices here
+            
 
 class DataAccessGroup:
     pass
